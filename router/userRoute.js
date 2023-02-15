b52eab73390ffffc98035ffe564632dffed5412e
Sujoy-cb <sujoyc070@gmail.com>
2023-02-15 23:25:39 +0600

    api check

--- router/userRoute.js
+++ router/userRoute.js
@@ -2,8 +2,20 @@ const express = require('express')
 const _ = express.Router()
 
 
-_.get('/', function(req,res){
-    res.send("Router set is ok")
+_.post('/', function(req,res){
+    let {email, fullname, password, isSubscribe} = req.body
+
+    if(!email){
+        res.json({error: "Please enter your email"})
+    }
+    if(!fullname){
+        res.json({error: "Please enter your fullname"})
+    }
+    if(!password){
+        res.json({error: "Please enter password"})
+    }
+
+    res.json({email, fullname, password, isSubscribe})
 })
 
 
