9baeae1df02047a544fe0789248e7effc97a03bc
Sujoy-cb <sujoyc070@gmail.com>
2023-02-15 23:27:49 +0600

    api check2

--- router/userRoute.js
+++ router/userRoute.js
@@ -6,13 +6,13 @@ _.post('/', function(req,res){
     let {email, fullname, password, isSubscribe} = req.body
 
     if(!email){
-        res.json({error: "Please enter your email"})
+        return res.json({error: "Please enter your email"})
     }
     if(!fullname){
-        res.json({error: "Please enter your fullname"})
+        return res.json({error: "Please enter your fullname"})
     }
     if(!password){
-        res.json({error: "Please enter password"})
+        return res.json({error: "Please enter password"})
     }
 
     res.json({email, fullname, password, isSubscribe})
